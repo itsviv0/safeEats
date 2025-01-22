096980e9d77de29ce4732e7e3e5beac00e939f89
itsviv0 <vivekganeshs12@gmail.com>
2025-01-22 22:56:33 +0530

    Added @react-native-ml-kit/text-recognition dependency and updated OCR service to use ML Kit for text recognition.

--- services/ocrService.ts
+++ services/ocrService.ts
@@ -1,9 +1,9 @@
-import Tesseract from "tesseract.js";
+import TextRecognition from "@react-native-ml-kit/text-recognition";
 
 export const extractText = async (imageUri: string): Promise<string> => {
   try {
-    let rawText = await Tesseract.recognize(imageUri);
-    return rawText.data.text;
+    const result = await TextRecognition.recognize(imageUri);
+    return result.text;
   } catch (error) {
     console.error("OCR Error:", error);
     throw error;
